dc019d014bbd8c73a216c7445140b9af0535f9d4
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-11-18 14:46:05 -0600

    Added Heroku and Database update connection

---
+++ Models/Enums/BTRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrace.Models.Enums
+{
+    public enum BTRoles
+    {
+        Admin,
+        ProjectManager,
+        Developer,
+        Submitter
+    }
+}
---
+++ Models/Enums/BTTicketType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrace.Models.Enums
+{
+    public enum BTTicketType
+    {
+        [Description("New Development")]
+        NewDevelopment,
+
+        [Description("Work Task")]
+        WorkTask,
+
+        [Description("Defect")]
+        Defect,
+
+        [Description("Change Request")]
+        ChangeRequest,
+
+        [Description("Enhancement")]
+        Enhancement,
+
+        [Description("General Task")]
+        GeneralTask
+    }
+}
